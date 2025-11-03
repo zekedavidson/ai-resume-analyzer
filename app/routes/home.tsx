19e571389baf239f262a8200d1920b005a546d00
zekedavidson <bhatiagary28@gmail.com>
2025-11-03 15:49:03 +0530

    updated home page, updated css, added sample resumes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,30 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+<Navbar/>
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
+
+    {resumes.map((resume) => (
+      <div key={resume.id}>
+        <h1>{resume.jobTitle}</h1>
+      </div>
+    ))}  
+
+  </main>;
 }
