20b6ee59fc401422437ab470a78dbfb2087971a8
zekedavidson <bhatiagary28@gmail.com>
2026-01-31 01:30:17 +0530

    Built resume card, added puter js script

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,17 +15,19 @@ export default function Home() {
 <Navbar/>
 
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
-    </section>
+    
 
-    {resumes.map((resume) => (
-      <div key={resume.id}>
-        <h1>{resume.jobTitle}</h1>
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+          {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume}/>
+          ))}
       </div>
-    ))}  
-
+    )}
+    </section>
   </main>;
 }
