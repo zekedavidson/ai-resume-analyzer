801c2e92bb8e92e8b7c0bf0aae56e07521d55df2
zekedavidson <bhatiagary28@gmail.com>
2026-01-31 20:01:35 +0530

    file uploading

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route('/auth', 'routes/auth.tsx')
+    route('/auth', 'routes/auth.tsx'),
+    route('/upload', 'routes/upload.tsx')
 ] satisfies RouteConfig;
---
+++ app/routes/upload.tsx
@@ -0,0 +1,76 @@
+import { type FormEvent, useState } from "react";
+import FileUploader from "~/components/FileUploader";
+import Navbar from "~/components/Navbar";
+
+const Upload = () => {
+    const [isProcesing, setIsProcessing] = useState(false);
+    const [statusText, setStatusText] = useState('');
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileSelect = (file: File | null) => {
+        setFile(file)
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest('form');
+        if (!form) return;
+        const formData = new FormData(form);
+
+        const companyName = formData.get('company-name');
+        const jobTitle = formData.get('job-title');
+        const jobDescription = formData.get('job-description');
+
+        console.log({
+            companyName,
+            jobTitle,
+            jobDescription
+        })
+    }
+
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+
+            <section className="main-section">
+                <div className='page-heading py-16'>
+                    <h1>Smart feedback for your dream job</h1>
+                    {isProcesing ? (
+                        <>
+                            <h2>{statusText}</h2>
+                            <img src="/images/resume-scan.gif" className="w-full" />
+                        </>
+                    ) : (
+                        <h2>Drop your resume for ATS score and improvement tips</h2>
+                    )}
+                    {!isProcesing && (
+                        <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
+                            <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                                <input type="text" name="company-name" placeholder="Company Name" id="company-name"></input>
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-title">Job Title</label>
+                                <input type="text" name="job-title" placeholder="Job Title" id="job-title"></input>
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="job-description">Job Description</label>
+                                <textarea rows={5} name="job-description" placeholder="Job Description" id="job-description"></textarea>
+                            </div>
+                            <div className="form-div">
+                                <label htmlFor="uploader">Upload Resume</label>
+                                <FileUploader onFileSelect={handleFileSelect} />
+                            </div>
+
+                            <button className="primary-button" type="submit">
+                                Analyze Resume
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Upload
