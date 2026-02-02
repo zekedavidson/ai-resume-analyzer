c62a8723903590389f61414eea5f6da1c728b612
zekedavidson <bhatiagary28@gmail.com>
2026-02-03 00:39:13 +0530

    implemented resume feedback page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -65,6 +65,7 @@ const Upload = () => {
         await kv.set(`resume: ${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
         console.log(data);
+        navigate(`/resume/${uuid}`);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
